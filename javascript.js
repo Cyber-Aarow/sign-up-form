ae4dab6763ded6e583fa63ece2f1bd8963c2ebc6
Cyber-Aarow <136511597+Cyber-Aarow@users.noreply.github.com>
2024-05-28 16:29:49 -0700

    Prevent non-alphabetical input for names.

--- javascript.js
+++ javascript.js
@@ -5,10 +5,26 @@ document.addEventListener("DOMContentLoaded", function(){
     });
 });
 
-let names = querySelectorAll(".name")
-names.forEach((element) =>{
-    element.addEventListener('input', function(){
-        let lettersOnly = element.value.replace(/[^a-zA-Z]/g, '');
-        element.value = lettersOnly;
-    });
-});
+
+//Prevent non-alphabetical input for names
+window.onload = function() {
+    firstName = document.querySelector("#first_name");
+    //Listen for keypress in the firstname input
+    firstName.addEventListener("keypress", function(e) {
+      char_regex = RegExp('[A-Za-z]')
+      //Prevent the key to be entered if it's not one of the alphabetic
+      if(!char_regex.test(e.key)) {
+        e.preventDefault();
+      }
+    })
+
+    lastName = document.querySelector("#last_name");
+    //Listen for keypress in the firstname input
+    lastName.addEventListener("keypress", function(e) {
+      char_regex = RegExp('[A-Za-z]')
+      //Prevent the key to be entered if it's not one of the alphabetic
+      if(!char_regex.test(e.key)) {
+        e.preventDefault();
+      }
+    })
+  }
